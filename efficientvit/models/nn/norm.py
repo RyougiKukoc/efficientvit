85ee8209a024a55bee1b4daad0ec320478e7fc6f
RyougiKukoc <st_stars@163.com>
2025-02-27 17:49:50 +0800

    not force import triton

--- efficientvit/models/nn/norm.py
+++ efficientvit/models/nn/norm.py
@@ -4,7 +4,10 @@ import torch
 import torch.nn as nn
 from torch.nn.modules.batchnorm import _BatchNorm
 
-from efficientvit.models.nn.triton_rms_norm import TritonRMSNorm2dFunc
+try:
+    from efficientvit.models.nn.triton_rms_norm import TritonRMSNorm2dFunc
+except ImportError:
+    pass
 from efficientvit.models.utils import build_kwargs_from_config
 
 __all__ = ["LayerNorm2d", "TritonRMSNorm2d", "build_norm", "reset_bn", "set_norm_eps"]
